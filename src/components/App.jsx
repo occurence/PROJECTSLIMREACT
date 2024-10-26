ebbc09dab30b6fb5d2b9ca42027647e94e30c15c
Arctaea <joycacao@gmail.com>
2024-10-26 18:31:19 +0800

    app lazy pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect, lazy } from 'react';
+// import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { useUser } from '../hooks/useUser';
+
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
+const ProductsPage = lazy(() => import('../pages/ProductsPage/ProductsPage'));
+
 export const App = () => {
+  // const dispatch = useDispatch();
+  const { isRefreshing } = useUser();
+
+
   return (
     <div>
       React homework template
