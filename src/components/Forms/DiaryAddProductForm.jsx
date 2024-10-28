74dd06d1d01cdf8c6815acff69bfc119f83650f8
Arctaea <joycacao@gmail.com>
2024-10-28 20:57:17 +0800

    layout for DiaryDateCalendar and add product

--- src/components/Forms/DiaryAddProductForm.jsx
+++ src/components/Forms/DiaryAddProductForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useUser } from '../../hooks/useUser';
 import main from '../App.module.css';
@@ -5,16 +6,36 @@ import main from '../App.module.css';
 export const DairyAddProductForm = () => {
     const dispatch = useDispatch();
     const { isLoggedIn } = useUser();
+    // const today = new Date().toDateString();
+    // const [date, setDate] = useState(today);
+    const today = new Date();
+    // const [date, setDate] = useState(today.toISOString());
+    const [date, setDate] = useState(today.toLocaleDateString('en-CA'));
+
     const handleSubmit = e => {
         e.preventDefault();
         const form = e.currentTarget;
         form.reset();
       };
-    return !isLoggedIn &&
+    return isLoggedIn &&
         <>
             <form className={main.form} onSubmit={handleSubmit} autoComplete="off">
-                <div>
-                    <button type="submit">Add</button>
+                {/* <label className={main.label} style={{color:"var(--orange)",height:"4em"}}>ADD PRODUCT</label> */}
+                <label className={main.label}>
+                    <input type="date" name="date" defaultValue={date.toString()} autoComplete="date" className={main.calendarLabel} />
+                </label>
+                <div className={main.row}>
+                    <label className={main.label}>Enter product name
+                        <input type="text" name="product" autoComplete="product" />
+                    </label>
+                    <div style={{position:"relative",display:"flex",gap:"80px"}}>
+                        <label className={main.label} style={{width:"30%",textAlign:"end"}}>Grams
+                            <input type="number" style={{width:"85%"}} name="grams" autoComplete="grams" />
+                        </label>
+                        <button type="submit" className={main.add}>
+                            <img src={require("../../images/insert.png")} />
+                        </button>
+                    </div>
                 </div>
             </form>
         </>
