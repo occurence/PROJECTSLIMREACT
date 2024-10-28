bae7f7879787d3484c8ddbd40a5a2de434b09082
Arctaea <joycacao@gmail.com>
2024-10-28 20:53:50 +0800

    layout for private calculator

--- src/components/Forms/CalculatorCalorieForm.jsx
+++ src/components/Forms/CalculatorCalorieForm.jsx
@@ -10,11 +10,40 @@ export const CalculatorCalorieForm = () => {
         const form = e.currentTarget;
         form.reset();
       };
-    return !isLoggedIn &&
+    return isLoggedIn &&
         <>
             <form className={main.form} onSubmit={handleSubmit} autoComplete="off">
-                <div>
-                    <button type="submit">Start losing weight</button>
+                {/* <label className={main.label} style={{color:"var(--orange)",height:"4em"}}>PRIVATE CALCULATOR CALORIE FORM</label> */}
+                <label className={main.label} style={{color:"var(--black)",height:"2em",fontSize:"34px",paddingTop:"80px",paddingBottom:"40px"}}>Calculate your daily calorie<br />intake right now</label>
+                <div className={main.row}>
+                    <label className={main.label}>Height *
+                        <input type="number" name="height" autoComplete="height" />
+                    </label>
+                    <label className={main.label}>Desired weight *
+                        <input type="number" name="desired" autoComplete="desired" />
+                    </label>
+                </div>
+                <div className={main.row}>
+                    <label className={main.label}>Age *
+                        <input type="number" name="age" autoComplete="age" />
+                    </label>
+                    <label className={main.label}>Blood type *
+                        <input type="text" name="blood" autoComplete="blood" />
+                    </label>
+                </div>
+                <div className={main.row}>
+                    <label className={main.label}>Current weight *
+                        <input type="number" name="weight" autoComplete="weight" />
+                    </label>
+                    <div className={main.radio}>
+                        <div className={main.radioItem}><input type="radio" name="option" value="o" defaultChecked={true} /><label className={main.radioLabel}>O</label></div>
+                        <div className={main.radioItem}><input type="radio" name="option" value="a" /><label className={main.radioLabel}>A</label></div>
+                        <div className={main.radioItem}><input type="radio" name="option" value="b" /><label className={main.radioLabel}>B</label></div>
+                        <div className={main.radioItem}><input type="radio" name="option" value="ab" /><label className={main.radioLabel}>AB</label></div>
+                    </div>
+                </div>
+                <div className={main.weightWrapper}>
+                    <button type="submit" id={main.weight}>Start losing weight</button>
                 </div>
             </form>
         </>
