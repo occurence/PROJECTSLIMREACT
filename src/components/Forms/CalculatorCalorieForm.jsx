f3828d1ac9a1edd58eded2800f1c30eab396320c
Arctaea <joycacao@gmail.com>
2024-10-30 23:51:15 +0800

    handleSubmit form dispatch for update on today operations

--- src/components/Forms/CalculatorCalorieForm.jsx
+++ src/components/Forms/CalculatorCalorieForm.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { useUser } from '../../hooks/useUser';
+import { update } from '../../redux/user/userOperations';
 import main from '../App.module.css';
 
 export const CalculatorCalorieForm = () => {
@@ -8,13 +9,29 @@ export const CalculatorCalorieForm = () => {
     const handleSubmit = e => {
         e.preventDefault();
         const form = e.currentTarget;
+        const height = Number(form.elements.height.value);
+        const age = Number(form.elements.age.value);
+        const weight = Number(form.elements.weight.value);
+        const desired = Number(form.elements.desired.value);
+        const blood = form.elements.blood.value;
+        const intake = 10 * weight + 6.25 * height - 5 * age - 161 - 10 * (weight - desired);
+        dispatch(
+            update({
+                height,
+                age,
+                weight,
+                desired,
+                blood,
+                intake,
+            })
+        );
         form.reset();
       };
     return isLoggedIn &&
         <>
             <form className={main.form} onSubmit={handleSubmit} autoComplete="off">
                 {/* <label className={main.label} style={{color:"var(--orange)",height:"4em"}}>PRIVATE CALCULATOR CALORIE FORM</label> */}
-                <label className={main.label} style={{color:"var(--black)",height:"2em",fontSize:"34px",paddingTop:"80px",paddingBottom:"40px"}}>Calculate your daily calorie<br />intake right now</label>
+                <label className={main.tag}>Calculate your daily calorie<br />intake right now</label>
                 <div className={main.row}>
                     <label className={main.label}>Height *
                         <input type="number" name="height" autoComplete="height" />
