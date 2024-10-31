3b4d679d33d48de3d81730395f744a7f565d33cf
Arctaea <joycacao@gmail.com>
2024-10-31 20:57:09 +0800

    add toast for operations

--- src/redux/product/productsOperations.js
+++ src/redux/product/productsOperations.js
@@ -1,5 +1,6 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-hot-toast';
 
 export const getProducts = createAsyncThunk(
   'products/fetchProductsAll',
@@ -18,8 +19,10 @@ export const getProductById = createAsyncThunk(
   async (productId, thunkAPI) => {
     try {
       const res = await axios.get(`/products/${productId}`);
+      toast.success(`Success ${res?.status}: \n${res?.data.message}`, {style:{backgroundColor:"var(--success)"}});
       return res.data;
     } catch (error) {
+      toast.error(`Error ${error.response?.status}: \n${error.response?.data?.message}`, {style:{backgroundColor:"var(--error)"}});
       return thunkAPI.rejectWithValue(error.message);
     }
   }
@@ -29,10 +32,12 @@ export const addProduct = createAsyncThunk(
   'products/addProduct',
   async ({ categories, weight, title, calories, groupBloodNotAllowed }, thunkAPI) => {
     try {
-      const response = await axios.post('/products', { categories, weight, title, calories, groupBloodNotAllowed });
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
+      const res = await axios.post('/products', { categories, weight, title, calories, groupBloodNotAllowed });
+      toast.success(`Success ${res?.status}: \n${res?.data.message}`, {style:{backgroundColor:"var(--success)"}});
+      return res.data;
+    } catch (error) {
+      toast.error(`Error ${error.response?.status}: \n${error.response?.data?.message}`, {style:{backgroundColor:"var(--error)"}});
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
@@ -41,10 +46,12 @@ export const deleteProduct = createAsyncThunk(
   'products/deleteProduct',
   async (productId, thunkAPI) => {
     try {
-      const response = await axios.delete(`/products/${productId}`);
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
+      const res = await axios.delete(`/products/${productId}`);
+      toast.success(`Success ${res?.status}: \n${res?.data.message}`, {style:{backgroundColor:"var(--success)"}});
+      return res.data;
+    } catch (error) {
+      toast.error(`Error ${error.response?.status}: \n${error.response?.data?.message}`, {style:{backgroundColor:"var(--error)"}});
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
@@ -54,7 +61,11 @@ export const editProduct = createAsyncThunk(
   async({ productId, categories, weight, title, calories, groupBloodNotAllowed }, thunkAPI) => {
       try{
           const res = await axios.patch(`/products/${productId}`, { productId, categories, weight, title, calories, groupBloodNotAllowed });
+          toast.success(`Success ${res?.status}: \n${res?.data.message}`, {style:{backgroundColor:"var(--success)"}});
           return res.data;
-      }catch(error){return thunkAPI.rejectWithValue(error.message);}
+        } catch (error) {
+          toast.error(`Error ${error.response?.status}: \n${error.response?.data?.message}`, {style:{backgroundColor:"var(--error)"}});
+          return thunkAPI.rejectWithValue(error.message);
+        }
   }
 );
