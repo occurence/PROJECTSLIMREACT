c58de0d958ad1372fd25c731959c6163a363c353
Arctaea <joycacao@gmail.com>
2024-10-31 20:58:42 +0800

    remove filter today

--- src/redux/today/todaysSelectors.js
+++ src/redux/today/todaysSelectors.js
@@ -1,15 +1,3 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { selectCalendar } from '../calendar/calendarSelectors';
-
 export const selectTodays = state => state.todays.items;
 export const selectIsLoadingTodays = state => state.todays.isLoading;
 export const selectIsErrorTodays = state => state.todays.isError;
-
-export const selectFilteredToday = createSelector(
-    [selectTodays, selectCalendar],
-    (todays, calendar) => {
-        return todays.filter(({ date }) => 
-            date.includes(calendar)
-        );
-    }
-);
