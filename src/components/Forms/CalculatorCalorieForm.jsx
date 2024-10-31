e236601a68da413526cd1d448ecf432b294cc149
Arctaea <joycacao@gmail.com>
2024-10-31 21:02:04 +0800

    add daily intake and radio rename

--- src/components/Forms/CalculatorCalorieForm.jsx
+++ src/components/Forms/CalculatorCalorieForm.jsx
@@ -1,11 +1,14 @@
 import { useDispatch } from 'react-redux';
 import { useUser } from '../../hooks/useUser';
-import { update } from '../../redux/user/userOperations';//todayOperations
+import { useToday } from '../../hooks/useToday';
+import { addToday } from '../../redux/today/todaysOperations';
 import main from '../App.module.css';
 
 export const CalculatorCalorieForm = () => {
     const dispatch = useDispatch();
-    const { isLoggedIn } = useUser();
+    const { user, isLoggedIn } = useUser();
+    const { year } = useToday();
+    const _id = user._id;
     const handleSubmit = e => {
         e.preventDefault();
         const form = e.currentTarget;
@@ -13,16 +16,28 @@ export const CalculatorCalorieForm = () => {
         const age = Number(form.elements.age.value);
         const weight = Number(form.elements.weight.value);
         const desired = Number(form.elements.desired.value);
-        const blood = form.elements.blood.value;
-        const intake = 10 * weight + 6.25 * height - 5 * age - 161 - 10 * (weight - desired);
+        const blood = Number(form.elements.blood.value);
+        const intake = 10 * weight + 6.25 * height - 5 * age - 161 - 10 * (desired);
+        console.log({
+            user: _id,
+            date: year,
+            height,
+            age,
+            weight,
+            weightDesired: desired,
+            blood,
+            dailyRate: intake,
+        });
         dispatch(
-            update({
-                height,
-                age,
-                weight,
-                desired,
-                blood,
-                intake,
+            addToday({
+                user: `${_id}`,
+                date: `${year}`,
+                height: `${height}`,
+                age: `${age}`,
+                weight: `${weight}`,
+                weightDesired: `${desired}`,
+                blood: `${blood}`,
+                dailyRate: `${intake}`,
             })
         );
         form.reset();
@@ -34,29 +49,29 @@ export const CalculatorCalorieForm = () => {
                 <label className={main.tag}>Calculate your daily calorie<br />intake right now</label>
                 <div className={main.row}>
                     <label className={main.label}>Height *
-                        <input type="number" name="height" autoComplete="height" />
+                        <input type="number" name="height" autoComplete="height" required />
                     </label>
                     <label className={main.label}>Desired weight *
-                        <input type="number" name="desired" autoComplete="desired" />
+                        <input type="number" name="desired" autoComplete="desired" required />
                     </label>
                 </div>
                 <div className={main.row}>
                     <label className={main.label}>Age *
-                        <input type="number" name="age" autoComplete="age" />
+                        <input type="number" name="age" autoComplete="age" required />
                     </label>
                     <label className={main.label}>Blood type *
-                        <input type="text" name="blood" autoComplete="blood" />
+                    <input type="text" name="blood" autoComplete="blood" disabled="disabled" />
                     </label>
                 </div>
                 <div className={main.row}>
                     <label className={main.label}>Current weight *
-                        <input type="number" name="weight" autoComplete="weight" />
+                        <input type="number" name="weight" autoComplete="weight" required />
                     </label>
                     <div className={main.radio}>
-                        <div className={main.radioItem}><input type="radio" name="option" value="o" defaultChecked={true} /><label className={main.radioLabel}>O</label></div>
-                        <div className={main.radioItem}><input type="radio" name="option" value="a" /><label className={main.radioLabel}>A</label></div>
-                        <div className={main.radioItem}><input type="radio" name="option" value="b" /><label className={main.radioLabel}>B</label></div>
-                        <div className={main.radioItem}><input type="radio" name="option" value="ab" /><label className={main.radioLabel}>AB</label></div>
+                        <div className={main.radioItem}><input type="radio" name="blood" value="1" defaultChecked={true} /><label className={main.radioLabel}>O</label></div>
+                        <div className={main.radioItem}><input type="radio" name="blood" value="2" /><label className={main.radioLabel}>A</label></div>
+                        <div className={main.radioItem}><input type="radio" name="blood" value="3" /><label className={main.radioLabel}>B</label></div>
+                        <div className={main.radioItem}><input type="radio" name="blood" value="4" /><label className={main.radioLabel}>AB</label></div>
                     </div>
                 </div>
                 <div className={main.weightWrapper}>
