3d959bfc062b80b1a82dd05a35077e3119a83b3f
Arctaea <joycacao@gmail.com>
2024-10-31 20:56:14 +0800

    remove filter today

--- src/hooks/useToday.js
+++ src/hooks/useToday.js
@@ -1,23 +1,21 @@
 import { useSelector } from 'react-redux';
-import { selectTodays, selectIsLoadingTodays, selectIsErrorTodays, selectFilteredToday } from '../redux/today/todaysSelectors';
+import { selectTodays, selectIsLoadingTodays, selectIsErrorTodays } from '../redux/today/todaysSelectors';
 import { selectMonth, selectYear, selectDay } from '../redux/calendar/calendarSelectors';
 
 export const useToday = () => {
     const isLoadingTodays = useSelector(selectIsLoadingTodays);
     const isErrorTodays= useSelector(selectIsErrorTodays);
     const todays = useSelector(selectTodays);
-    const filteredTodays = useSelector(selectFilteredToday);
-    const mmddyyyy = useSelector(selectMonth);
-    const yyyymmdd = useSelector(selectYear);
-    const ddmmyyyy = useSelector(selectDay);
+    const month = useSelector(selectMonth);
+    const year = useSelector(selectYear);
+    const day = useSelector(selectDay);
 
   return {
     isLoadingTodays,
     isErrorTodays,
     todays,
-    filteredTodays,
-    mmddyyyy,
-    yyyymmdd,
-    ddmmyyyy,
+    month,
+    year,
+    day,
   };
 };
