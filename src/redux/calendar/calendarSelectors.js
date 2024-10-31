0175ca423adff0ff9502403554bd89a7d19d45e5
Arctaea <joycacao@gmail.com>
2024-10-31 19:31:03 +0800

    calendar per format

---
+++ src/redux/calendar/calendarSelectors.js
@@ -0,0 +1,4 @@
+export const selectCalendar = state =>  state.calendar;
+export const selectMonth = state =>  state.calendar.mmddyyyy;
+export const selectYear = state =>  state.calendar.yyyymmdd;
+export const selectDay = state =>  state.calendar.ddmmyyyy;
