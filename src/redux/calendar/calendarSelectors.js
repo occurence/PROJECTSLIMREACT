b47d8759cdd9c2a18a74a7624fc611045cd54bbb
Arctaea <joycacao@gmail.com>
2024-11-01 20:28:51 +0800

    redux state for default calendar format

--- src/redux/calendar/calendarSelectors.js
+++ src/redux/calendar/calendarSelectors.js
@@ -1,14 +1,5 @@
 export const selectCalendar = state =>  state.calendar;
-// export const selectMonth = state =>  state.calendar.mmddyyyy;
-// export const selectYear = state =>  state.calendar.yyyymmdd;
-// export const selectDay = state =>  state.calendar.ddmmyyyy;
-// export const selectMonth = state =>  state.calendar.month;
-// export const selectYear = state =>  state.calendar.year;
-// export const selectDay = state =>  state.calendar.day;
-// export const selectMonth = state => state.calendar.calendar.month;
-// export const selectYear = state => state.calendar.calendar.year;
-// export const selectDay = state => state.calendar.calendar.day;
-
+export const selectDefault = state => state.calendar.default;
 export const selectMonth = state => state.calendar.month;
 export const selectYear = state => state.calendar.year;
 export const selectDay = state => state.calendar.day;
