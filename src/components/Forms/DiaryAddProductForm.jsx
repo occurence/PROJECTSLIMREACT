f708902de62cf09a97dcd2146323e262d0cf0331
Arctaea <joycacao@gmail.com>
2024-11-03 19:23:16 +0800

    mongoose format for parenthesis \? and dispatch update for today schema products as body and date as params

--- src/components/Forms/DiaryAddProductForm.jsx
+++ src/components/Forms/DiaryAddProductForm.jsx
@@ -4,10 +4,12 @@ import { DebounceInput } from 'react-debounce-input';
 import { setFilter } from '../../redux/filter/filterSlice';
 import { useUser } from '../../hooks/useUser';
 import { useProduct } from '../../hooks/useProduct';
+import { useToday } from '../../hooks/useToday';
 import { DiaryDateCalendar } from './DiaryDateCalendar';
 import main from '../App.module.css';
 
 import { DiaryProductsList } from './DiaryProductsList/DiaryProductsList';
+import { consumeProduct } from '../../redux/today/todaysOperations';
 
 export const DairyAddProductForm = () => {
     const dispatch = useDispatch();
@@ -15,10 +17,28 @@ export const DairyAddProductForm = () => {
     const { isLoadingProducts, isErrorProducts, filteredProducts } = useProduct();
     const [productInput, setProductInput] = useState('');
     const [isDropdown, setIsDropdown] = useState(false);
-
+    const [selectedProductId, setSelectedProductId] = useState(null);
+    const { defaultDate, year } = useToday();
+    // console.log(filteredProducts.length);
     const handleFilterChange = e => {
         setProductInput(e.target.value);
         dispatch(setFilter(e.target.value));
+        // const inputValue = e.target.value;
+        // const inputFormat = inputValue
+        // .replace("(", "\\\\(?")
+        // .replace(")", "\\\\)?")
+        // if (inputValue.includes("(")) {inputFormat += "\\s*\\(?";}
+        // else if (inputValue.includes(")")) {inputFormat += "\\)?";}
+        // + (inputValue.includes("(") ? "\\\\(?" : "") 
+        // + (inputValue.includes(")") ? "\\)?" : "");
+        // + (inputValue.includes("(") ? "\\\\(?" : "") 
+        // + (inputValue.includes(")") ? "\\)?" : "");
+        // console.log(inputFormat)
+        // let formatInput;
+        // if(e.target.value.includes("(")){formatInput="\\(?"}
+        // else if(e.target.value.includes(")")){formatInput="\\)?"}
+        // console.log(formatInput);
+        // const formatInput = e.target.value.includes("(") && "\\(?";
     }
 
     const handleSubmit = e => {
@@ -26,21 +46,59 @@ export const DairyAddProductForm = () => {
         const form = e.currentTarget;
         const productName = form.elements.product.value;
         const grams = form.elements.grams.value;
+        const formattedDate = new Date(defaultDate).toLocaleDateString('en-CA').split('T')[0];
         // alert(productName, grams);
-        console.log(`${productName}: ${grams}`);
-        // dispatch(consumeProduct());
+        // console.log(`${productName}: ${grams}`);
+        console.log(`${formattedDate},${productName}: ${grams}, Product ID: ${selectedProductId}`);
+        
+        dispatch(consumeProduct({
+            todayDate: defaultDate,
+            // products: [{
+            // productId: selectedProductId,
+            productId: selectedProductId,
+            productName,
+            grams
+            // }]
+        }));
+        // dispatch(consumeProduct({
+        //     todayDate: formattedDate,
+        //     body: {
+        //         products: [{
+        //             productId: selectedProductId,
+        //             productName,
+        //             grams
+        //         }]
+        //     }
+        // }));
+        setSelectedProductId(null);
         setProductInput('');
         dispatch(setFilter(''));
         setIsDropdown(false);
         form.reset();
     }
 
-    const handleDropdown = e => {
-        setProductInput(e);
-        dispatch(setFilter(e));
-        setIsDropdown(false);
-    }
+    // const handleDropdown = e => {
+    //     setProductInput(e);
+    //     dispatch(setFilter(e));
+    //     setIsDropdown(false);
+    //     const inputFormat = e
+    //     .replace("(", "\\\\(?")
+    //     .replace(")", "\\\\)?")
+    //     console.log(inputFormat);
+    // }
 
+    const handleDropdown = (title, id) => {
+        setSelectedProductId(id);
+        setProductInput(title);
+        dispatch(setFilter(title));
+        setIsDropdown(false);
+        const inputFormat = title
+        .replace("(", "\\\\(?")
+        .replace(")", "\\\\)?")
+        console.log(inputFormat);
+        console.log(`Product ID: ${id}`);
+    }
+    
     return isLoggedIn &&
         <>
             <form className={main.form} style={{left:"25px",height:"70%",gap:"5px"}} onSubmit={handleSubmit} autoComplete="off">
@@ -60,7 +118,8 @@ export const DairyAddProductForm = () => {
                                 {filteredProducts.map((filteredProduct) => (
                                     <li className={main.dropdownItem}
                                         key={filteredProduct._id}
-                                        onClick={() => {handleDropdown(filteredProduct.title);setIsDropdown(!isDropdown)}}
+                                        // onClick={() => {handleDropdown(filteredProduct.title);setIsDropdown(!isDropdown)}}
+                                        onClick={() => { handleDropdown(filteredProduct.title, filteredProduct._id) }}
                                     >
                                         {filteredProduct.title}
                                     </li>
