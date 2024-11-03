0edab48b006271c9a2f9f9704d6aa70bd506395a
Arctaea <joycacao@gmail.com>
2024-11-03 19:16:38 +0800

    selectDefault calendar state name from default to defaultDate

--- src/redux/calendar/calendarSelectors.js
+++ src/redux/calendar/calendarSelectors.js
@@ -1,5 +1,5 @@
 export const selectCalendar = state =>  state.calendar;
-export const selectDefault = state => state.calendar.default;
+export const selectDefault = state => state.calendar.defaultDate;
 export const selectMonth = state => state.calendar.month;
 export const selectYear = state => state.calendar.year;
 export const selectDay = state => state.calendar.day;
